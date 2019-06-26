920223e3df998a2de8b0da6c2689a6b397f1c3c9
Haim D <haim11293@aol.com>
2019-06-26 02:44:13 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -52,7 +52,7 @@ end
 
 def invalid_command
   # code invalid_command here
-  print "Please enter a valid command"
+  puts "Please enter a valid command"
 end
 
 #####################################################
